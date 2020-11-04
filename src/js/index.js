8867567932d0cba64697652eadc6fbc676d22af1
richardso21 <richard.so21@sitechhs.com>
2020-11-04 11:07:41 -0500

    basic pagination

--- src/js/index.js
+++ src/js/index.js
@@ -4,11 +4,9 @@ const DOMSelectors = {
   grid: document.querySelector(".movie-grid"),
   searchMovieTitle: document.querySelector("#search-movie-name"),
   searchMovieSubmit: document.querySelector("#search-movie-submit"),
+  next: document.querySelector("#next"),
+  prev: document.querySelector("#prev"),
 };
-
-const apiKey = "e0ddda2b8af45601dfef37a0cd439cac";
-
-const defaultQuery = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=vote_average.desc&include_adult=false&include_video=false&page=1&vote_count.gte=1000&vote_average.gte=8`;
 
 function display(results) {
   // clear all on movie grid
@@ -51,30 +49,58 @@ function display(results) {
   });
 }
 
-async function fetchDisplayQuery(query) {
-  const response = await fetch(query);
-  const data = await response.json();
-  display(data.results);
+async function fetchDisplayQuery(query, page) {
+  try {
+    const response = await fetch(query);
+    const data = await response.json();
+    // if it has no data, throw error to be catched
+    if (data.results.length === 0) throw Error;
+    // else, display
+    display(data.results);
+  } catch (error) {
+    console.log(error);
+    return 1;
+  }
 }
 
-function search() {
-  const search = DOMSelectors.searchMovieTitle.value;
-  const searchQuery = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${search}&page=1&include_adult=false`;
-  fetchDisplayQuery(searchQuery);
+function makeQuery(searchBool, page, search = "") {
+  const apiKey = "e0ddda2b8af45601dfef37a0cd439cac";
+  const defaultQuery = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=vote_average.desc&include_adult=false&include_video=false&page=${page}&vote_count.gte=1000&vote_average.gte=8`;
+  const searchQuery = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${search}&page=${page}&include_adult=false`;
+  if (!searchBool) return defaultQuery;
+  else return searchQuery;
 }
 
 function init() {
-  try {
-    // display default query
-    fetchDisplayQuery(defaultQuery);
+  let page = 1;
+  let search;
+  let searchBool = 0;
 
-    // event listener for search
-    DOMSelectors.searchMovieSubmit.addEventListener("click", (e) => {
-      e.preventDefault();
-      search();
-    });
-  } catch (error) {
-    console.error(error);
-  }
+  // display default query
+  const currentQuery = makeQuery(searchBool, page, search);
+  fetchDisplayQuery(currentQuery);
+
+  // event listener for search
+  DOMSelectors.searchMovieSubmit.addEventListener("click", (e) => {
+    e.preventDefault();
+    searchBool = 1;
+    search = DOMSelectors.searchMovieTitle.value;
+    const currentQuery = makeQuery(searchBool, page, search)
+    fetchDisplayQuery(currentQuery);
+  });
+
+  DOMSelectors.next.addEventListener("click", async function() {
+    page++;
+    const currentQuery = makeQuery(searchBool, page, search);
+    const res = await fetchDisplayQuery(currentQuery, page);
+    if (res) page--;
+  });
+
+  DOMSelectors.prev.addEventListener("click", async function() {
+    page--;
+    const currentQuery = makeQuery(searchBool, page, search);
+    const res = await fetchDisplayQuery(currentQuery);
+    if (res) page++;
+  });
 }
 init();
